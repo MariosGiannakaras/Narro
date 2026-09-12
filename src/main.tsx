9027f8f574257e1559b838f7ae388f179506ee83
mariog <28479056+MariosGiannakaras@users.noreply.github.com>
2026-09-12 21:21:03 +0300

    M5 theme: install runtime in main webview

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { ThemeRuntimeProvider } from "./ThemeRuntime";
 import { TimerSessionProjection } from "./TimerSessionProjection";
 import "./taskScheduleDialog.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-    <TimerSessionProjection label="Main" />
+    <ThemeRuntimeProvider>
+      <App />
+      <TimerSessionProjection label="Main" />
+    </ThemeRuntimeProvider>
   </React.StrictMode>,
 );
